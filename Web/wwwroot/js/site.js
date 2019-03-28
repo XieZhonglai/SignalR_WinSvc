9b0d61362ada1e0a11bef3799778af1227d44fac
XieZhonglai <xiezl@lingqidz.com>
2019-03-28 10:54:57 +0800

    .

---
+++ Web/wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("http://localhost:7788/dzcHub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+
+
+
+connection.on("addMessage", function (name, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = name + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("Send", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
